595082a94611f9dd52c1f6734d3f8f02c8164e8c
xiabaiyang <baiyangfe@gmail.com>
2017-04-11 21:54:37 +0800

    安全权限设置

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ var Sequelize = require('sequelize');
 // var env = process.env.NODE_ENV || 'development';
 var env = 'production';
 
-var config = require(__dirname + '/../config/database.json')[env];
+var config = require(__dirname + '/../config/' + env + '/database.json');
 
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
